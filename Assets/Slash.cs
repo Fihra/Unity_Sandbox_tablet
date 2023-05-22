0828f50f7b7c854dc12558a8c05a19bdf7666087
Fihra <fabrofabian@gmail.com>
2023-05-21 14:12:25 -1000

    created slash script

---
+++ Assets/Slash.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slash : MonoBehaviour
+{
+
+    [SerializeField]
+    private int damage = 1;
+
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public int GetDamage()
+    {
+        return damage;
+    }
+
+    void Start()
+    {
+        Invoke("SlashLifeSpan", 0.1f);
+    }
+
+    void SlashLifeSpan()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

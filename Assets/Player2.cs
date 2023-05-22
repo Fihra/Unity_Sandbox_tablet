576faedb8fb81dd07092063517a4f75e23b767bd
Fihra <fabrofabian@gmail.com>
2023-05-21 14:04:35 -1000

    created another test player for slashing combo

---
+++ Assets/Player2.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    //For rotating character
+    [SerializeField]
+    private float rotationSpeed = 100f;
+    private float horizontal;
+    private float vertical;
+    private Vector2 newPosition;
+
+    [SerializeField]
+    private float moveSpeed = 1f;
+
+    [SerializeField]
+    float dashSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void RotateDirection()
+    {
+
+        if(newPosition != Vector2.zero)
+        {
+            //Parameters: desired forward direction, 
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, newPosition);
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            rb.MoveRotation(rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        newPosition = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);    
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+        RotateDirection();
+    }
+}

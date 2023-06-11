27f76c0adf16c6cc3229fbe0b2bb4985241d3036
Fihra <fabrofabian@gmail.com>
2023-06-11 13:52:35 -1000

    dash system implemented

--- Assets/Player2.cs
+++ Assets/Player2.cs
@@ -16,16 +16,29 @@ public class Player2 : MonoBehaviour
     [SerializeField]
     private int attackPower = 1;
 
+    private float activeMoveSpeed;
+    private float dashCounter;
+    private float dashCoolCounter;
+
     [SerializeField]
     private float moveSpeed = 1f;
 
     [SerializeField]
     float dashSpeed = 1f;
 
+    [SerializeField]
+    float dashCooldown = 1f;
+
+    [SerializeField]
+    float dashLength = 0.5f;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        activeMoveSpeed = moveSpeed;
     }
 
     public int GetAttackPower()
@@ -45,18 +58,52 @@ public class Player2 : MonoBehaviour
         }
     }
 
+    void Dash()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        Debug.Log("Dashing");
+        Debug.Log("Dash Counter: " + dashCounter);
+        Debug.Log("Dash Cool Counter: " + dashCoolCounter);
+        {
+            if(dashCounter <= 0 && dashCoolCounter <= 0) 
+            {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+            }
+        }
+
+        if(dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if(dashCounter <= 0)
+            {
+                activeMoveSpeed = moveSpeed;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+
+        if(dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
+        }
+
+    }
+
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
 
-        newPosition = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);    
+        newPosition = new Vector2(horizontal * activeMoveSpeed, vertical * activeMoveSpeed);
+
+        Dash();
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+        rb.velocity = new Vector2(horizontal * activeMoveSpeed, vertical * activeMoveSpeed);
         RotateDirection();
     }
 }
